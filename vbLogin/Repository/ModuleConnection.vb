242860fc9cff9c2f4346b0d1ee0dff21a56913fa
virusphp <packercyber@gmail.com>
2023-06-15 00:38:23 +0700

    visual basic login mysql

---
+++ vbLogin/Repository/ModuleConnection.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Data.Odbc
+
+Module ModuleConnection
+    Public Connect As OdbcConnection
+    Public DA As OdbcDataAdapter
+    Public DS As DataSet
+    Public CMD As OdbcCommand
+    Public RD As OdbcDataReader
+
+    Public MYDB As String
+
+    Public Sub koneksi()
+        MYDB = "Driver={Mysql 8.0.33 driver};database=db_aplikasi;server=localhost;uid=megono;password=password123$$"
+        Connect = New OdbcConnection(MYDB)
+        If Connect.State = ConnectionState.Closed Then Connect.Open()
+    End Sub
+End Module
